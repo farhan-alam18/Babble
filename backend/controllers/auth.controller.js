e75f265e367114aa50235dc0b9ed9edbb9774a08
farhan-alam18 <farhanalam1113@gmail.com>
2024-09-01 00:07:56 +0530

    AUTH - SignUp Done

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,63 @@
+import { generateTokenandSetCookie } from "../lib/generateToken.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
 export const signup = async (req, res) => {
-  res.json({
-    data: "You have hit the signup endpoint",
-  });
+  try {
+    const { fullName, username, email, password } = req.body;
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ error: "Invalid email ID" });
+    }
+
+    const existingUser = await User.findOne({ username: username });
+    if (existingUser) {
+      return res.status(400).json({ error: "Username is already taken" });
+    }
+
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ error: "Email is already taken" });
+    }
+
+    if(password.length < 6){
+      return res.status(400).json({error: "Password must be at least 6 characters long"})
+    }
+
+    // Hashing Password :
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateTokenandSetCookie(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        email: newUser.email,
+        followers: newUser.followers,
+        following: newUser.following,
+        profileImg: newUser.profileImg,
+        coverImg: newUser.coverImg,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
 export const login = async (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
