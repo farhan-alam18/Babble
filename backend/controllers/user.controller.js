e71183a7185547c110f65f713c6f0233bd0e55c1
farhan-alam18 <farhanalam1113@gmail.com>
2024-09-02 19:10:22 +0530

    GET Suggested Users

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -52,13 +52,42 @@ export const followUnfollowUser = async (req, res) => {
         to: userToModify._id,
       });
 
-      await newNotification.save()
+      await newNotification.save();
 
       // TODO : return the id of the user as a response
       res.status(200).json({ message: "User followed successfully" });
     }
   } catch (error) {
     console.log("Error in followUnfollowUser : ", error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getSuggestedUsers = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    const followedUsers = await User.findById(userId).select("following");
+    const users = await User.aggregate([
+      {
+        $match: {
+          _id: { $ne: userId },
+        },
+      },
+      { $sample: { size: 10 } },
+    ]);
+
+    const filteredUsers = users.filter(
+      (user) => !followedUsers.following.includes(user._id)
+    );
+
+    const suggestedUsers = filteredUsers.slice(0, 4);
+    suggestedUsers.forEach((user) => (user.password = null));
+
+    res.status(200).json(suggestedUsers);
+
+  } catch (error) {
+    console.log("Error in getSuggestedUsers : ", error.message);
     res.status(500).json({ error: error.message });
   }
 };
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { followUnfollowUser, getUserProfile } from "../controllers/user.controller.js";
+import { followUnfollowUser, getSuggestedUsers, getUserProfile } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
