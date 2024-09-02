3d4e53510a82cf94fb08d26a9b35862b83c2d1a0
farhan-alam18 <farhanalam1113@gmail.com>
2024-09-02 21:48:19 +0530

    DELETE Post Done

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -31,3 +31,30 @@ export const createPost = async (req, res) => {
     console.log("Error in createPost controller : ", error);
   }
 };
+
+export const deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+
+    if (post.user.toString() !== req.user._id.toString()) {
+      return res
+        .status(401)
+        .json({ error: "You are not authorized to delete this post" });
+    }
+
+    if (post.img) {
+      const imgId = post.img.split("/").pop().split(".")[0];
+      await cloudinary.uploader.destroy(imgId);
+    }
+
+    await Post.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Post successfully deleted" });
+  } catch (error) {
+    console.log("Error in deletePost controller : ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
--- backend/routes/posts.routes.js
+++ backend/routes/posts.routes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { createPost } from "../controllers/post.controller.js";
+import { createPost, deletePost } from "../controllers/post.controller.js";
 
 const router = express.Router()
 
 router.post("/create",protectRoute,createPost)
 // router.post("/like/:id",protectRoute,likeUnlikePost)
 // router.post("/comment/:id",protectRoute,commentPost)
-// router.delete("/",protectRoute,deletePost)
+router.delete("/:id",protectRoute,deletePost)
 
 export default router
