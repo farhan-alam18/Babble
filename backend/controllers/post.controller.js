3b59bad62dcfcf3b69cbad6f66ec8ca565958b83
farhan-alam18 <farhanalam1113@gmail.com>
2024-09-03 07:08:56 +0530

    GET All Posts Done

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -122,3 +122,27 @@ export const likeUnlikePost = async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 };
+
+export const getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find()
+      .sort({ createdAt: -1 })
+      .populate({
+        path: "user",
+        select: "-password",
+      })
+      .populate({
+        path: "comments.user",
+        select: "-password",
+      });
+
+    if (posts.length === 0) {
+      return res.status(200).json({ message: "There are no posts" });
+    }
+
+    res.status(200).json(posts);
+  } catch (error) {
+    console.log("Error in getAllPosts : ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
--- backend/routes/posts.routes.js
+++ backend/routes/posts.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { commentPost, createPost, deletePost, likeUnlikePost } from "../controllers/post.controller.js";
+import { commentPost, createPost, deletePost, getAllPosts, likeUnlikePost } from "../controllers/post.controller.js";
 
 const router = express.Router()
 
+router.get("/all",protectRoute,getAllPosts)
 router.post("/create",protectRoute,createPost)
 router.post("/like/:id",protectRoute,likeUnlikePost)
 router.post("/comment/:id",protectRoute,commentPost)
