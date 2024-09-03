951783341cd5a865c1c45c22558c725b0084c95f
farhan-alam18 <farhanalam1113@gmail.com>
2024-09-03 11:49:59 +0530

    GET Following Posts

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -172,3 +172,40 @@ export const likedPosts = async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 };
+
+export const followingPosts = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const user = await User.findById(userId);
+
+    if (!user) return res.status(404).json({ error: "User not found" });
+
+    const following = await user.following;
+
+    const feedPosts = await Post.find({ user: { $in: following } })
+      .sort({ createdAt: -1 })
+      .populate({
+        path: "comments.user",
+        select: "-password",
+      });
+
+    res.status(200).json(feedPosts);
+  } catch (error) {
+    console.log("Error in follwingPosts : ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+
+export const userPosts = async (req,res) => {
+   try {
+    const {username} = req.params
+
+    const user = await User.findOne({username})
+
+    
+    
+   } catch (error) {
+    
+   }
+}
--- backend/routes/posts.routes.js
+++ backend/routes/posts.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { commentPost, createPost, deletePost, getAllPosts, likedPosts, likeUnlikePost } from "../controllers/post.controller.js";
+import { commentPost, createPost, deletePost, followingPosts, getAllPosts, likedPosts, likeUnlikePost } from "../controllers/post.controller.js";
 
 const router = express.Router()
 
 router.get("/all",protectRoute,getAllPosts)
 router.get("/likes/:id",protectRoute,likedPosts)
+router.get("/following",protectRoute,followingPosts)
+router.get("user/:username", protectRoute, userPosts)
 router.post("/create",protectRoute,createPost)
 router.post("/like/:id",protectRoute,likeUnlikePost)
 router.post("/comment/:id",protectRoute,commentPost)
