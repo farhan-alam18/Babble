8f6fef5ca2633b9d2abbebcf9473a1de10ddaaab
farhan-alam18 <farhanalam1113@gmail.com>
2024-09-07 05:26:05 +0530

    Client - Follow Users Done

---
+++ frontend/src/hooks/useFollow.jsx
@@ -0,0 +1,35 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
+
+const useFollow = () => {
+  const queryClient = useQueryClient();
+
+  const { mutate: follow, isPending } = useMutation({
+    mutationFn: async (userId) => {
+      try {
+        const res = await fetch(`/api/users/follow/${userId}`, {
+          method: "POST",
+        });
+
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+        return data;
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    },
+    onSuccess: () => {
+      toast.success("User followed successfully");
+      Promise.all([
+        queryClient.invalidateQueries({ queryKey: ["suggestedUsers"] }),
+        queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+      ]);
+    },
+  });
+
+  return { follow, isPending };
+};
+
+export default useFollow;
