f5712983573ead6301cb5763a30d9760d95ea00c
farhan-alam18 <farhanalam1113@gmail.com>
2024-09-07 20:52:39 +0530

    Code Optimizations

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -204,26 +204,26 @@ export const followingPosts = async (req, res) => {
 };
 
 export const userPosts = async (req, res) => {
-  try {
-    const { username } = req.params;
+	try {
+		const { username } = req.params;
 
-    const user = await User.findOne({ username });
-    if (!user) res.status(404).json({ error: "User not found" });
+		const user = await User.findOne({ username });
+		if (!user) return res.status(404).json({ error: "User not found" });
 
-    const posts = await Post.find({ user: user._id })
-      .sort({ createdAt: -1 })
-      .populate({
-        path: "user",
-        select: "-password",
-      })
-      .populate({
-        path: "comments.user",
-        select: "-password",
-      });
+		const posts = await Post.find({ user: user._id })
+			.sort({ createdAt: -1 })
+			.populate({
+				path: "user",
+				select: "-password",
+			})
+			.populate({
+				path: "comments.user",
+				select: "-password",
+			});
 
-    res.status(200).json(posts);
-  } catch (error) {
-    console.log("Error in userPosts : ", error.message);
-    res.status(500).json({ error: "Internal server error" });
-  }
+		res.status(200).json(posts);
+	} catch (error) {
+		console.log("Error in getUserPosts controller: ", error);
+		res.status(500).json({ error: "Internal server error" });
+	}
 };
