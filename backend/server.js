c0d9ce0682d5f623cf3d76fe6767c83dc575be41
farhan-alam18 <farhanalam1113@gmail.com>
2024-09-07 21:25:16 +0530

    Errors Fixed

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.use("/api/notifications", notificationRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.length("*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
