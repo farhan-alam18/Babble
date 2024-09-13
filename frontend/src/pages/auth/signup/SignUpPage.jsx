f1755ab31570dc7e0984fa10d0248bfc47006450
Farhan Alam <farhanalam1113@gmail.com>
2024-09-13 22:16:36 +0530

    Update SignUpPage.jsx

--- frontend/src/pages/auth/signup/SignUpPage.jsx
+++ frontend/src/pages/auth/signup/SignUpPage.jsx
@@ -8,10 +8,11 @@ import { FaUser } from "react-icons/fa";
 import { MdPassword } from "react-icons/md";
 import { MdDriveFileRenameOutline } from "react-icons/md";
 
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 
 const SignUpPage = () => {
+  const queryClient = useQueryClient()
   const [formData, setFormData] = useState({
     email: "",
     username: "",
@@ -41,6 +42,7 @@ const SignUpPage = () => {
     },
     onSuccess: () => {
       toast.success("Account created successfully");
+      queryClient.invalidateQueries({ queryKey: ["authUser"] });
     },
   });
 
